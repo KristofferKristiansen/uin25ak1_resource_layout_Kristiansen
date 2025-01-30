6b4eddaae203eba94c68c49bc3377ccb26392cee
KristofferKristiansen <mattiskk@hiof.no>
2025-01-30 18:06:16 +0100

    Ferdig
    finito:)

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let menuHTML = ""
+let infoHTML = ""
+let selectedCategory = []
+let selectedCategory1 = []
+
+resources.map((mappe, index) => menuHTML +=
+`<li id="${mappe.category}" class="item"><button onclick="myFunction('${mappe.category}')">${mappe.category}</button></li>` 
+)
+
+document.getElementById("liste").innerHTML = menuHTML
+
+myFunction("HTML")
+
+function myFunction(navn) {
+   selectedCategory1 = resources.filter((info) => info.category == navn)
+   selectedCategory = selectedCategory1[0]
+
+    console.log(selectedCategory)
+
+    infoHTML += `<article class="item">
+                <h2 id="topptext">${selectedCategory.category}</h2>
+                <span id ="text">${selectedCategory.text}</span>    
+                <ul class="linker">
+                    ${selectedCategory.sources.map(source =>
+                    `<li><a href ="${source.url}">${source.title} </a> </li>`).join("")}
+                </ul>
+            </article>`
+            document.getElementById("liste").innerHTML = menuHTML + infoHTML
+            infoHTML = ""
+
+}
